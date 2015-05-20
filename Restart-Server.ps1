5440eed4670e5fd3f280a44259cb27f71ee36043
coldenough <vrednopolezno@gmail.com>
2015-05-20 12:20:14 -0700

    Source code of the script

---
+++ Restart-Server.ps1
@@ -0,0 +1,20 @@
+﻿function restart-server {
+  [CmdletBinding() ]
+  param (
+    [parameter (ValueFromPipeline=$true ,
+    ValueFromPipelineByPropertyName =$true) ]
+    [string ]$computer= ""
+  )
+  PROCESS {
+    $cred = Get-Credential
+    $comp = Get-WmiObject Win32_OperatingSystem -ComputerName $computer 
+                                                -Credential $cred
+    $ret = $comp. Reboot()
+    if ($ret.ReturnValue -eq 0){
+      Write-Host "Restarting $computer succeeded."
+    }
+    else {
+      Write-Host "Restarting $computer failed"
+    }
+  }
+} 
