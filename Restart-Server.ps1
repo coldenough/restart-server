a2e4f9c08ccadba7db581ee1e5e9d1bd4c839c95
coldenough <vrednopolezno@gmail.com>
2015-05-21 10:56:23 -0700

    Replaced Test-Path to Ping-Host

--- Restart-Server.ps1
+++ Restart-Server.ps1
@@ -28,7 +28,7 @@ function restart-server {
   }
   PROCESS {
     foreach ($computer in $computerName) {
-      if (Test-Path $computer) {
+      if (Ping-Host $computer) {
         Write-Host "$computer is pingable"
 <#        $comp = Get-WmiObject Win32_OperatingSystem -ComputerName $computer `
                                                     -Credential $cred
